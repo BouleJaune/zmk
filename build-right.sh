53d8d12c02904caf2d5a3d390c082cf0137c8aaf
BouleJaune <amthierry81@live.fr>
2025-07-18 21:33:52 +0200

    update

--- build-right.sh
+++ build-right.sh
@@ -2,7 +2,7 @@
 #### build sweep firmware ####
 
 cd /workspace/app
-west build  -d /workspace/zmk-config/right -b nice_nano_v2 -p -o=-j6 -- -DSHIELD=cradio_right -DZMK_CONFIG=/workspace/zmk-config/config 
+west build  -d /workspace/zmk-config/right -b nice_nano_v2 -p -o=-j6 -- -DSHIELD=cradio_right -DZMK_CONFIG=/workspace/zmk-config/config
 cp /workspace/zmk-config/right/zephyr/zmk.uf2 /workspace/zmk-config/right-zmk.uf2
 cp /workspace/zmk-config/right/zephyr/zmk.uf2 /mnt/
 rm -rf /workspace/zmk-config/right/
